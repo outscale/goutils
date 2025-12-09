259b587b0d1a4b8c70b9f36837b5e5ce1684ccda
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2025-12-09 12:50:13 +0000

    📝 README

--- sdk/generics/maps/maps.go
+++ sdk/generics/maps/maps.go
@@ -1,9 +1,21 @@
 package maps
 
-func FromSlice[K comparable, V any, S any](s []S, mapFn func(S) (K, V)) map[K]V {
+func Map[K comparable, V any, KK comparable, VV any](in map[K]V, fn func(k K, v V) (KK, VV, bool)) map[KK]VV {
+	out := make(map[KK]VV, len(in))
+	for k, v := range in {
+		nk, nv, ok := fn(k, v)
+		if !ok {
+			continue
+		}
+		out[nk] = nv
+	}
+	return out
+}
+
+func FromSlice[K comparable, V any, S any](s []S, fn func(S) (K, V)) map[K]V {
 	m := make(map[K]V, len(s))
 	for _, k := range s {
-		key, val := mapFn(k)
+		key, val := fn(k)
 		m[key] = val
 	}
 	return m
