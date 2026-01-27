d95db5597c9727f46cdbb3b36e666335c024c2ea
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2026-01-27 09:41:18 +0000

    ✨ feat(metadata): add instance type, omi id & placement info

--- sdk/metadata/metadata.go
+++ sdk/metadata/metadata.go
@@ -17,15 +17,23 @@ var DefaultService = NewService(http.DefaultClient)
 const (
 	MetadataServer = "http://169.254.169.254/latest/meta-data/"
 
-	SubRegion  = "placement/availability-zone"
-	InstanceID = "instance-id"
+	SubRegion        = "placement/availability-zone"
+	PlacementServer  = "placement/server"
+	PlacementCluster = "placement/cluster"
+	InstanceID       = "instance-id"
+	OMIID            = "ami-id"
+	InstanceType     = "instance-type"
 )
 
 // Metadata is the metadata returned by the metadata server.
 type Metadata struct {
-	InstanceID string
-	Region     string
-	SubRegion  string
+	InstanceID   string
+	OMIID        string
+	InstanceType string
+	Region       string
+	SubRegion    string
+	Cluster      string
+	Server       string
 }
 
 // Service is a metadata service.
@@ -73,21 +81,57 @@ func (s *Service) fetchInstanceID(ctx context.Context) (string, error) {
 	return s.fetch(ctx, InstanceID)
 }
 
+func (s *Service) fetchOMIID(ctx context.Context) (string, error) {
+	return s.fetch(ctx, OMIID)
+}
+
+func (s *Service) fetchInstanceType(ctx context.Context) (string, error) {
+	return s.fetch(ctx, InstanceType)
+}
+
+func (s *Service) fetchPlacementCluster(ctx context.Context) (string, error) {
+	return s.fetch(ctx, PlacementCluster)
+}
+
+func (s *Service) fetchPlacementServer(ctx context.Context) (string, error) {
+	return s.fetch(ctx, PlacementServer)
+}
+
 // Fetch fetches metadata from the metadata server.
 func (s *Service) Fetch(ctx context.Context) (Metadata, error) {
 	instanceID, err := s.fetchInstanceID(ctx)
 	if err != nil {
 		return Metadata{}, err
 	}
-	availabilityZone, err := s.fetchSubRegion(ctx)
+	az, err := s.fetchSubRegion(ctx)
 	if err != nil {
 		return Metadata{}, err
 	}
-	region := availabilityZone[0 : len(availabilityZone)-1]
+	omi, err := s.fetchOMIID(ctx)
+	if err != nil {
+		return Metadata{}, err
+	}
+	instanceType, err := s.fetchInstanceType(ctx)
+	if err != nil {
+		return Metadata{}, err
+	}
+	cluster, err := s.fetchPlacementCluster(ctx)
+	if err != nil {
+		return Metadata{}, err
+	}
+	server, err := s.fetchPlacementServer(ctx)
+	if err != nil {
+		return Metadata{}, err
+	}
+	region := az[0 : len(az)-1]
 	return Metadata{
-		InstanceID: instanceID,
-		Region:     region,
-		SubRegion:  availabilityZone,
+		InstanceID:   instanceID,
+		OMIID:        omi,
+		InstanceType: instanceType,
+		Region:       region,
+		SubRegion:    az,
+		Cluster:      cluster,
+		Server:       server,
 	}, nil
 }
 
@@ -113,4 +157,24 @@ func GetRegion(ctx context.Context) (string, error) {
 // GetInstanceID fetches the instance ID from the metadata server.
 func GetInstanceID(ctx context.Context) (string, error) {
 	return DefaultService.fetchInstanceID(ctx)
+}
+
+// GetInstanceType fetches the instance type from the metadata server.
+func GetInstanceType(ctx context.Context) (string, error) {
+	return DefaultService.fetchInstanceType(ctx)
+}
+
+// GetOMIID fetches the OMI ID from the metadata server.
+func GetOMIID(ctx context.Context) (string, error) {
+	return DefaultService.fetchOMIID(ctx)
+}
+
+// GetPlacementCluster fetches the cluster where the VM is located.
+func GetPlacementCluster(ctx context.Context) (string, error) {
+	return DefaultService.fetchPlacementCluster(ctx)
+}
+
+// GetPlacementServer fetches the physical server where the VM is located.
+func GetPlacementServer(ctx context.Context) (string, error) {
+	return DefaultService.fetchPlacementServer(ctx)
 }
